90838e06d1fe84a08e049ba91a84c88130a4fb30
Timur Sergeev <82363203+LuvTrippin@users.noreply.github.com>
2024-02-24 17:25:29 +0500

    init: start react-app-project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return <footer className="page-footer purple lighten-4">
+    <div className="footer-copyright purple lighten-4">
+      <div className="container">
+      © {new Date().getFullYear()} Copyright Text
+      <a className="grey-text text-lighten-4 right" href="https://github.com/LuvTrippin">Coded by Timur Sergeev</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer};
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="purple lighten-4">
+    <div className="nav-wrapper">
+      <a href="https://github.com/LuvTrippin/react-shop" className="brand-logo">React Shop</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li><a href="https://github.com/LuvTrippin">GitHub</a></li>
+      </ul>
+    </div>
+  </nav>
+}
+
+export {Header};
