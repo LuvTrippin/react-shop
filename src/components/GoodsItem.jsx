b62263a074bee1e2e01a04d26dfa60bbd44fd98d
Timur Sergeev <82363203+LuvTrippin@users.noreply.github.com>
2024-02-25 14:04:34 +0500

    fix: fix empty image url problem

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -7,15 +7,18 @@ function GoodsItem(props) {
         full_background,
     } = props;
 
+    const handleImageError = (event) => {
+        event.target.src = `https://placehold.co/257x257?text=${name}\\n${price}`;
+    };
+
     return (
     <div className="card" id={id}>
         <div className="card-image">
-          <img src={full_background ? full_background : `https://placehold.co/257.5x257.5?text=${name}\n${price}`} alt={name} />
-          
-          <button className="btn-floating halfway-fab waves-effect waves-light red"><i className="material-icons">add</i></button>
+          <img src={full_background} alt={name} onError={handleImageError} />
+          <button className="btn-floating halfway-fab waves-effect waves-light purple lighten-4"><i className="material-icons">add</i></button>
         </div>
         <div className="card-content">
-          <p>{description}</p>
+          <p>{description ? description : name}</p>
         </div>
     </div>
     )
