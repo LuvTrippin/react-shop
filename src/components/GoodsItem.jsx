167eb69499817cd5fd2dcf53574c6c4961228bc4
Timur Sergeev <82363203+LuvTrippin@users.noreply.github.com>
2024-02-25 17:37:29 +0500

    feat: add Cart functional

--- src/components/GoodsItem.jsx
+++ src/components/GoodsItem.jsx
@@ -8,17 +8,22 @@ function GoodsItem(props) {
     } = props;
 
     const handleImageError = (event) => {
-        event.target.src = `https://placehold.co/257x257?text=${name}\\nцена: ${price}`;
+        event.target.src = `https://placehold.co/257x257?text=${name}\\nцена: ${price} Руб.`;
     };
+
+    const handleClick = () => {
+        props.addToBasket({id, name, price});
+    }
 
     return (
     <div className="card" id={id}>
         <div className="card-image">
           <img src={full_background} alt={name} onError={handleImageError} />
-          <button className="btn-floating halfway-fab waves-effect waves-light purple lighten-4"><i className="material-icons">add</i></button>
+          <button className="btn-floating halfway-fab waves-effect waves-light purple lighten-4" onClick={handleClick}><i className="material-icons">add</i></button>
         </div>
         <div className="card-content">
           <p>{description ? description : name}</p>
+          <p><b>Цена:</b> <span className="right"><b>{price} Руб.</b></span></p>
         </div>
     </div>
     )
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,19 @@
-import {useState, useEffect} from 'react';
-import {API_KEY, API_URL} from '../config'
-import {Preloader} from './Preloader'
-import {GoodsList} from './GoodsList'
+import { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config'
+import { Preloader } from './Preloader'
+import { GoodsList } from './GoodsList'
+import { Cart } from './Cart';
 
 function Shop() {
     const [goods, setGoods] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [order, setOrder] = useState([]);
+
+    const addToBasket = (item) =>  {
+        if (!order.filter(good => good.id === item.id).length > 0) {
+            setOrder([item, ...order]);
+        }
+    };
 
     useEffect(() => {
         fetch(API_URL, {
@@ -20,7 +28,8 @@ function Shop() {
     }, []);
  
     return <main className="container content" >
-        {loading ? <Preloader/> : <GoodsList goods={goods} />}
+        <Cart quantity={order.length} />
+        {loading ? <Preloader/> : <GoodsList goods={goods} addToBasket={addToBasket} />}
     </main>
 }
 
